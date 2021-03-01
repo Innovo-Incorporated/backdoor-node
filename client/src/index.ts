2cb069715dc2960c625ead9a7840c3132d8b2ec5
Madhav Anand <madhav1004@icloud.com>
2021-03-01 20:59:57 +0530

    sockets are working.

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello")
+import {io, Socket} from "socket.io-client";
+
+class Client {
+    private readonly socket: Socket;
+
+    constructor() {
+        const socket = io("http://localhost:3000")
+
+        this.socket = socket
+
+        socket.on("connect", ()=>{
+            this.sendMessage("hola")
+        })
+    }
+
+    sendMessage(data: any){
+        this.socket.emit("command", JSON.stringify(data))
+    }
+}
+
+const client = new Client()
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,58 @@
-console.log("hello")
+import express from "express";
+import http from "http";
+import socket from "socket.io";
+import prompts from "prompts";
+
+
+
+class Server {
+    constructor() {
+        const app = express()
+
+        app.get('/', (req, res) => {
+            res.json({
+                hello: "hello hello hello robert kiyosaki here."
+            })
+        })
+
+        const server = new http.Server(app)
+
+        // @ts-ignore
+        const io = socket(server)
+
+        io.on('connection', (socket: any)=>{
+            console.log("user connected.")
+            this.listenCommands()
+
+            socket.on("command", (data: any)=>{
+                console.log(data)
+            })
+        })
+
+        server.listen(3000, ()=> console.log("Server up on http://localhost:3000"))
+    }
+
+    async listenCommands(){
+        while (true){
+           const {value} = await this.prompt("Get started by a command")
+           if (value === "quit"){
+               break
+           }else {
+               console.log(value)
+           }
+        }
+    }
+
+    async prompt(message: string){
+        const response = await prompts({
+            type: "text",
+            name: "value",
+            message: message,
+            validate: value => !value ? "Please enter a value" : true
+        })
+
+        return response
+    }
+}
+
+const server = new Server()
